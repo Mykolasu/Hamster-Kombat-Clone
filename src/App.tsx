77312e893d6f54fee04dcfd4af2d4c192d0eee77
Mykola Batrak <kolyaun@live.com>
2024-08-13 23:44:41 +0300

    levelnames, levelindex

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
 import './App.css'
 import Hamster from './icons/Hamster';
+import { useState } from 'react';
 
 function App() {
+
+  const levelNames = [
+    "Bronze",      // from 0 to 4999 coins
+    "Silver",      // from 5000 coins to 24,999 coins
+    "Gold",        // from 25,000 coins to 99,999 coins
+    "Platinum",    // from 100,000 coins to 999,999 coins
+    "Diamond",     // from 1,000,000 coins to 1,999,999 coins
+    "Epic",        // from 2,000,000 coins to 9,999,999 coins
+    "Legendary",   // from 10,000,000 coins to 49,999,999 coins
+    "Master",      // from 50,000,000 coins to 99,999,999 coins
+    "GrandMaster", // from 100,000,000 coins to 999,999,999 coins
+    "Lord"         // from 1,000,000,000 coins to infinity
+  ];
+
+  const [levelIndex, setLevelIndex] = useState(6);
 
   return (
     <div className='bg-black flex justify-center'>
@@ -15,6 +31,14 @@ function App() {
             </div>
             <div>
               <p className='text-sm'>Mykolka (CEO)</p>
+            </div>
+          </div>
+
+          <div className='flex items-center justify-between space-x-4 mt-1'>
+            <div className='flex items-center w-1/3'>
+              <div className='w-full'>
+
+              </div>
             </div>
           </div>
 
